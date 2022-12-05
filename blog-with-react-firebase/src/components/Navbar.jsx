a44c42600030830d020b3db2560d3599943000f9
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-05 14:14:14 +0900

    feature:tailwindcssの導入

--- blog-with-react-firebase/src/components/Navbar.jsx
+++ blog-with-react-firebase/src/components/Navbar.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">ホーム</Link>
+      <Link to="/" className="m-4">
+        ホーム
+      </Link>
       <Link to="/createpost">記事投稿</Link>
       <Link to="/login">ログイン</Link>
     </nav>
