f06c863f69ecdd828549a26eedad321ee5a4920c
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-05 14:03:14 +0900

    feature:ルーティングを作成

--- blog-with-react-firebase/src/App.jsx
+++ blog-with-react-firebase/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import CreatePost from "./components/CreatePost";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/createpost" element={<CreatePost />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
