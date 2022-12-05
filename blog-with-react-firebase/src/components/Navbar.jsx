36750026c9f5d59a676a51e4eb548c06503f46cc
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-05 14:26:37 +0900

    feature:Navbarにスタイルを実装

--- blog-with-react-firebase/src/components/Navbar.jsx
+++ blog-with-react-firebase/src/components/Navbar.jsx
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/" className="m-4">
+    <nav className="flex justify-center items-center h-12 bg-yellow-600 gap-11 text-white">
+      <Link to="/" className="hover:text-blue-400">
         ホーム
       </Link>
-      <Link to="/createpost">記事投稿</Link>
-      <Link to="/login">ログイン</Link>
+      <Link to="/createpost" className="hover:text-blue-400">
+        記事投稿
+      </Link>
+      <Link to="/login" className="hover:text-blue-400">
+        ログイン
+      </Link>
     </nav>
   );
 };
