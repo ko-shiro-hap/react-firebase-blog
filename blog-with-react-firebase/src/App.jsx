d7f2216d594d1d0a9b45fac465cd5a2c632981f2
ko-shiro-hap <koushirou.iino@icloud.com>
2022-12-06 10:50:14 +0900

    feature:ログイン機能を実装

--- blog-with-react-firebase/src/App.jsx
+++ blog-with-react-firebase/src/App.jsx
@@ -8,13 +8,15 @@ import Logout from "./components/Logout";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
         <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </Router>
--- blog-with-react-firebase/src/components/Login.jsx
+++ blog-with-react-firebase/src/components/Login.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth, provider } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+
+  const loginInWithGoogle = () => {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 };
 
 export default Login;
